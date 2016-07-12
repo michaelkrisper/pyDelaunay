6c07a9e75663360c01bec0d7d6af28aaaeda0246
Michael Krisper <michael.krisper@gmail.com>
2016-07-12 23:27:36 +0200

    fixed bug in determinant, added many tests

--- delaunay.py
+++ delaunay.py
@@ -1,41 +1,19 @@
 #! /usr/bin/env python3
 # coding=utf-8
 """
-A basic implementation of delaunay triangulation.
-
-usage:
-    d = DelaunayMap([(0, 0, 0), (1, 0, 1), (0, 1, 1)])
-    d[0.25, 0.25]
- -> 0.5
+An implementation of delaunay triangulation.
 """
-
-from collections import Counter
-from typing import List, Tuple
-
+from collections import Counter, namedtuple
+from math import isclose
 import time
+from typing import Tuple, Iterable, List
 
 __author__ = "Michael Krisper"
 __email__ = "michael.krisper@gmail.com"
-__date__ = "2016-06-14"
+__date__ = "2016-07-12"
+__python_version__ = "3.5"
 
-
-class Point:
-    def __init__(self, x: float, y: float, z: float):
-        self.X = x
-        self.Y = y
-        self.Z = z
-
-    def __repr__(self):
-        return "({X},{Y},{Z})".format_map(vars(self))
-
-    def __sub__(self, other: 'Point') -> 'Point':
-        return Point(self.X - other.X, self.Y - other.Y, self.Z - other.Z)
-
-    def __eq__(self, other):
-        return (self.X, self.Y, self.Z) == (other.X, other.Y, other.Z)
-
-    def __hash__(self):
-        return hash((self.X, self.Y, self.Z))
+Point = namedtuple("Point", "X Y Z")
 
 
 class Edge:
@@ -44,105 +22,112 @@ class Edge:
         self.P2 = p2
 
     def __hash__(self):
-        return hash(self.P1) ^ hash(self.P2)
+        return hash((min(self.P1, self.P2), max(self.P1, self.P2)))
 
     def __eq__(self, other):
-        return (self.P1 == other.P1 and self.P2 == other.P2) or (self.P2 == other.P1 and self.P1 == other.P2)
+        return (self.P1, self.P2) == (other.P1, other.P2) or (self.P1, self.P2) == (other.P2, other.P1)
 
     def __repr__(self):
-        return "({P1}->{P2})".format_map(vars(self))
+        return "({P1} -> {P2})".format_map(vars(self))
 
 
 class Triangle:
     def __init__(self, p1: Point, p2: Point, p3: Point):
-        if (p1.X == p2.X == p3.X) or (p1.Y == p2.Y == p3.Y):
-            raise Exception("triangle is coplanar --> cannot extrapolated by a plane.")
+        if p1.X == p2.X == p3.X or p1.Y == p2.Y == p3.Y:
+            raise Exception(
+                "triangle is coplanar --> cannot extrapolated by a plane.\np1:{p1}\np2:{p2}\np3:{p3}".format_map(
+                    vars()))
 
         self.P1 = p1
         self.P2 = p2
         self.P3 = p3
 
     def __repr__(self):
-        return "({P1},{P2},{P3})".format_map(vars(self))
+        return "<{P1}, {P2}, {P3}>".format_map(vars(self))
 
     def contains_in_circumcircle(self, p: Point) -> bool:
-        p0 = self.P1 - p
-        p1 = self.P2 - p
-        p2 = self.P3 - p
-        det01 = p0.X * p1.Y - p0.Y * p1.X
-        det12 = p1.X * p2.Y - p1.Y * p2.X
-        det20 = p2.X * p0.Y - p2.Y * p0.X
-        return (p0.X ** 2 + p0.Y ** 2) * det12 + (p1.X ** 2 + p1.Y ** 2) * det20 + (p2.X ** 2 + p2.Y ** 2) * det01 > 0
+        p1 = self.P1
+        p2 = self.P2
+        p3 = self.P3
+        p4 = p
+        a, b, c = p1.X - p4.X, p1.Y - p4.Y, (p1.X ** 2 - p4.X ** 2) + (p1.Y ** 2 - p4.Y ** 2)
+        d, e, f = p2.X - p4.X, p2.Y - p4.Y, (p2.X ** 2 - p4.X ** 2) + (p2.Y ** 2 - p4.Y ** 2)
+        g, h, i = p3.X - p4.X, p3.Y - p4.Y, (p3.X ** 2 - p4.X ** 2) + (p3.Y ** 2 - p4.Y ** 2)
+        det = a * (e * i - f * h) + b * (f * g - d * i) + c * (d * h - e * g)
+        return det > 0 and not isclose(d, 0)
 
     def shares_vertex(self, tr: 'Triangle') -> bool:
         return {self.P1, self.P2, self.P3} & {tr.P1, tr.P2, tr.P3}
 
     def is_inside(self, x: float, y: float) -> bool:
-        v0 = self.P3 - self.P1
-        v1 = self.P2 - self.P1
-        v2 = Point(x, y, 0) - self.P1
+        v0 = Point(self.P3.X - self.P1.X, self.P3.Y - self.P1.Y, 0)
+        v1 = Point(self.P2.X - self.P1.X, self.P2.Y - self.P1.Y, 0)
+        v2 = Point(x - self.P1.X, y - self.P1.Y, 0)
         dot00 = v0.X ** 2 + v0.Y ** 2
         dot01 = v0.X * v1.X + v0.Y * v1.Y
         dot02 = v0.X * v2.X + v0.Y * v2.Y
         dot11 = v1.X ** 2 + v1.Y ** 2
         dot12 = v1.X * v2.X + v1.Y * v2.Y
-        inv_denom = 1 / dot00 * dot11 - dot01 ** 2
+        inv_denom = 1.0 / (dot00 * dot11 - dot01 ** 2)
         u = (dot11 * dot02 - dot01 * dot12) * inv_denom
         v = (dot00 * dot12 - dot01 * dot02) * inv_denom
-        return u >= 0 and v >= 0 and u + v <= 1
+        return (u > 0 or isclose(u, 0)) and (v > 0 or isclose(v, 0)) and (u + v < 1 or isclose(u + v, 1))
 
-
-class Plane:
-    def __init__(self, tr: Triangle):
-        ab = tr.P2 - tr.P1
-        ac = tr.P3 - tr.P1
-        self.X = ab.Y * ac.Z - ab.Z * ac.Y
-        self.Y = ab.Z * ac.X - ab.X * ac.Z
-        self.Z = ab.X * ac.Y - ab.Y * ac.X
-        self.W = tr.P1.X * self.X + tr.P1.Y * self.Y + tr.P1.Z * self.Z
-
-    def __repr__(self):
-        return "({X},{Y},{Z},{W})".format_map(vars(self))
+    def interpolate(self, x: float, y: float) -> float:
+        ab = Point(self.P2.X - self.P1.X, self.P2.Y - self.P1.Y, self.P2.Z - self.P1.Z)
+        ac = Point(self.P3.X - self.P1.X, self.P3.Y - self.P1.Y, self.P3.Z - self.P1.Z)
+        X = ab.Y * ac.Z - ab.Z * ac.Y
+        Y = ab.Z * ac.X - ab.X * ac.Z
+        Z = ab.X * ac.Y - ab.Y * ac.X
+        W = self.P1.X * X + self.P1.Y * Y + self.P1.Z * Z
+        return (W - X * x - Y * y) / Z
 
 
 class DelaunayMap:
-    def __init__(self, _points: List[Tuple[float, float, float]]):
-        assert len(_points) >= 3
-        points = [Point(*p) for p in _points]
+    def __init__(self, *_points: Iterable[Tuple[float, float, float]]):
+        points = sorted(list(Point(p[0], p[1], p[2]) for p in _points))
+        if len(points) != len({(p.X, p.Y) for p in points}):
+            raise Exception("(X,Y) coordinates of the points must be unique!")
+        if len(points) < 3:
+            raise Exception("There must be at least 3 points!")
+
         min_x, min_y = min(p.X for p in points), min(p.Y for p in points)
         max_x, max_y = max(p.X for p in points), max(p.Y for p in points)
-        supertriangle = Triangle(Point(min_x - 1, min_y - 1, 0), Point(max_x * 3, 0, 0), Point(min_x - 1, max_y * 3, 0))
+        supertriangle = Triangle(Point(min_x - 1, min_y - 1, 0), Point(max_x * 3, min_y - 1, 0),
+                                 Point(min_x - 1, max_y * 3, 0))
         triangles = [supertriangle]
-
+        point_count = 0
         for p in points:
-            container_triangles = [tr for tr
-                                   in triangles
-                                   if tr.contains_in_circumcircle(p)]
+            point_count += 1
+            container_triangles = [tr for tr in triangles if tr.contains_in_circumcircle(p)]
             edges = []  # type: List[Edge]
             for tr in container_triangles:
                 triangles.remove(tr)
                 edges += [Edge(tr.P1, tr.P2), Edge(tr.P2, tr.P3), Edge(tr.P3, tr.P1)]
+            unique_edges = (edge for edge, count in Counter(edges).items() if count == 1)
+            new_triangles = (Triangle(e.P1, e.P2, p) for e in unique_edges)
+            triangles += new_triangles
 
-            unique_edges = [edge
-                            for edge, count in Counter(edges).items()
-                            if count == 1]
+            if len(triangles) != 2 * (point_count + 3) - 2 - 3:
+                raise Exception(
+                    "Triangulation invariant violated! Triangle count and point count doesn't fit together.")
 
-            triangles += [Triangle(e.P1, e.P2, p) for e in unique_edges]
-
-        self.triangles = [tr
-                          for tr in triangles
-                          if not tr.shares_vertex(supertriangle)]
+        self._triangles = [tr for tr in triangles if not tr.shares_vertex(supertriangle)]
 
     def __getitem__(self, xy: Tuple[float, float]) -> float:
         x, y = xy
-        for tr in self.triangles:  # type: Triangle
+        for tr in self._triangles:  # type: Triangle
             if tr.is_inside(x, y):
-                pl = Plane(tr)
-                return (pl.W - pl.X * x - pl.Y * y) / pl.Z
+                return tr.interpolate(x, y)
 
 
 if __name__ == '__main__':
+    with open("map.csv") as f:
+        points = [tuple(float(x) for x in line.strip().split(",")) for line in f.readlines()]
+
     start = time.clock()
-    d = DelaunayMap([(0, 0, 0), (1, 0, 1), (0, 1, 1)])
-    result = d[0.25, 0.25]
+    d = DelaunayMap(*points)
+
+    for x, y, z in points:
+        assert z == d[x, y]
     print(time.clock() - start)
--- test.py
+++ test.py
@@ -1,42 +1,100 @@
 import unittest
-from math import isclose
-
 from delaunay import DelaunayMap
 
 
 class Test(unittest.TestCase):
-    def setUp(self):
-        self.points = [
-            (0, 0, 0),
-            (1, 0, 0.5),
-            (0, 1, 0.5),
-            (1, 1, 1),
-
-        ]
-        self.d = DelaunayMap(self.points)
-
     def test_usage(self):
-        d = DelaunayMap([(0, 0, 0), (1, 0, 1), (0, 1, 1)])
+        d = DelaunayMap((0, 0, 0), (1, 0, 1), (0, 1, 1))
         assert d[0.25, 0.25] == 0.5
 
+    def test0(self):
+        d = DelaunayMap((0, 0, 0), (0, 1, 0), (1, 0, 0))
+        assert d[0, 0] == 0
+        assert d[1, 0] == 0
+        assert d[0, 1] == 0
+        assert d[0.5, 0.5] == 0
+        assert d[0.25, 0.25] == 0
+
     def test1(self):
-        assert self.d[0.25, 0.25] == 0.25
+        d = DelaunayMap((0, 0, 1), (0, 1, 1), (1, 0, 1))
+        assert d[0, 0] == 1
+        assert d[1, 0] == 1
+        assert d[0, 1] == 1
+        assert d[0.5, 0.5] == 1
+        assert d[0.25, 0.25] == 1
 
     def test2(self):
-        assert self.d[0.5, 0.5] == 0.5
-
-    def test3(self):
-        assert self.d[0.5, 0] == 0.25
-
-    def test4(self):
-        assert self.d[0, 0.5] == 0.25
-
-    def test5(self):
-        assert self.d[0.125, 0.125] == 0.125
+        d = DelaunayMap((0, 0, 0), (0, 1, 1), (1, 0, 1))
+        assert d[0, 0] == 0
+        assert d[1, 0] == 1
+        assert d[0, 1] == 1
+        assert d[0.5, 0.5] == 1
+        assert d[0, 0.5] == 0.5
+        assert d[0.5, 0] == 0.5
+        assert d[0.25, 0.25] == 0.5
+        assert d[0.125, 0.125] == 0.25
+        assert d[0.25, 0] == 0.25
+        assert d[0, 0.25] == 0.25
 
     def test_fixpoints(self):
-        for p in self.points:
-            assert self.d[p[0], p[1]] == p[2]
+        with open("map.csv") as f:
+            points = [tuple(float(x) for x in line.strip().split(",")) for line in f.readlines()]
+        d = DelaunayMap(*points)
+
+        for x, y, z in points:
+            assert z == d[x, y]
+
+    def test_Simple_DelaunayMap(self):
+        assert 0 == DelaunayMap([0, 0, 0], [1, 0, 0], [0, 1, 0])[0.25, 0.25]
+
+    def test_DelaunayMapTriangle(self):
+        map = DelaunayMap([0, 0, 0], [1, 0, 1], [0, 1, 2])
+
+        # fixed points
+        assert 0 == map[0, 0]
+        assert 1 == map[1, 0]
+        assert 2 == map[0, 1]
+
+        # interpolations
+        assert 0.5 == map[0.5, 0]
+        assert 1 == map[0, 0.5]
+        assert 1.5 == map[0.5, 0.5]
+        assert 0.25 == map[0.25, 0]
+        assert 0.5 == map[0, 0.25]
+        assert 0.75 == map[0.25, 0.25]
+        assert 0.75 == map[0.75, 0]
+        assert 1.5 == map[0, 0.75]
+
+    def test_DelaunayMapPlane(self):
+        map = DelaunayMap([0, 0, 0, 3], [1, 0, 1], [0, 1, 2], [1, 1, 3])
+        # fixed points
+        assert 0 == map[0, 0]
+        assert 1 == map[1, 0]
+        assert 2 == map[0, 1]
+        assert 3 == map[1, 1]
+
+        # interpolations
+        assert 0.5 == map[0.5, 0]
+        assert 1 == map[0, 0.5]
+        assert 2 == map[1, 0.5]
+        assert 2.5 == map[0.5, 1]
+
+        assert 1.5 == map[0.5, 0.5]
+
+        assert 0.75 == map[0.25, 0.25]
+        assert 2.25 == map[0.75, 0.75]
+
+        assert 1.75 == map[0.25, 0.75]
+        assert 1.25 == map[0.75, 0.25]
+
+    def test_Delaunay_DuplicatePoints(self):
+        try:
+            DelaunayMap((0, 0, 0), (1, 0, 1), (1, 1, 3), (0, 1, 2), (1, 1, 5))
+        except Exception:
+            pass
+
+    def test_call_with_list(self):
+        DelaunayMap(*[(0, 0, 0, 4), (1, 0, 1), (1, 1, 3), (0, 1, 2)])
 
 
 if __name__ == '__main__':
