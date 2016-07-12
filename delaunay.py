e53427ebe60fdcc3c27f3097830a46270c41b28a
Michael Krisper <michael.krisper@gmail.com>
2016-07-13 00:28:09 +0200

    performance

--- delaunay.py
+++ delaunay.py
@@ -3,7 +3,7 @@
 """
 An implementation of delaunay triangulation.
 """
-from collections import Counter, namedtuple
+from collections import Counter
 from math import isclose
 import time
 from typing import Tuple, Iterable, List
@@ -13,8 +13,24 @@ __email__ = "michael.krisper@gmail.com"
 __date__ = "2016-07-12"
 __python_version__ = "3.5"
 
-Point = namedtuple("Point", "X Y Z")
 
+class Point:
+    def __init__(self, x: float, y: float, z: float):
+        self.X = float(x)
+        self.Y = float(y)
+        self.Z = float(z)
+
+    def __repr__(self):
+        return "({X:.7}, {Y:.7}, {Z:.7})".format_map(vars(self))
+
+    def __eq__(self, other):
+        return isclose(self.X, other.X) and isclose(self.Y, other.Y) and isclose(self.Z, other.Z)
+
+    def __hash__(self):
+        return hash((self.X, self.Y, self.Z))
+
+    def __lt__(self, other):
+        return (self.X, self.Y, self.Z).__lt__((other.X, other.Y, other.Z))
 
 class Edge:
     def __init__(self, p1: Point, p2: Point):
@@ -37,7 +53,6 @@ class Triangle:
             raise Exception(
                 "triangle is coplanar --> cannot extrapolated by a plane.\np1:{p1}\np2:{p2}\np3:{p3}".format_map(
                     vars()))
-
         self.P1 = p1
         self.P2 = p2
         self.P3 = p3
@@ -45,29 +60,30 @@ class Triangle:
     def __repr__(self):
         return "<{P1}, {P2}, {P3}>".format_map(vars(self))
 
-    def contains_in_circumcircle(self, p: Point) -> bool:
-        p1 = self.P1
-        p2 = self.P2
-        p3 = self.P3
-        p4 = p
-        a, b, c = p1.X - p4.X, p1.Y - p4.Y, (p1.X ** 2 - p4.X ** 2) + (p1.Y ** 2 - p4.Y ** 2)
-        d, e, f = p2.X - p4.X, p2.Y - p4.Y, (p2.X ** 2 - p4.X ** 2) + (p2.Y ** 2 - p4.Y ** 2)
-        g, h, i = p3.X - p4.X, p3.Y - p4.Y, (p3.X ** 2 - p4.X ** 2) + (p3.Y ** 2 - p4.Y ** 2)
-        det = a * (e * i - f * h) + b * (f * g - d * i) + c * (d * h - e * g)
-        return det > 0 and not isclose(d, 0)
-
     def shares_vertex(self, tr: 'Triangle') -> bool:
         return {self.P1, self.P2, self.P3} & {tr.P1, tr.P2, tr.P3}
 
+    def contains_in_circumcircle(self, p: Point) -> bool:
+        p1X, p1Y = self.P1.X, self.P1.Y
+        p2X, p2Y = self.P2.X, self.P2.Y
+        p3X, p3Y = self.P3.X, self.P3.Y
+        p4X, p4Y = p.X, p.Y
+        p4X2, p4Y2 = p4X ** 2, p4Y ** 2
+        a, b, c = p1X - p4X, p1Y - p4Y, (p1X ** 2 - p4X2) + (p1Y ** 2 - p4Y2)
+        d, e, f = p2X - p4X, p2Y - p4Y, (p2X ** 2 - p4X2) + (p2Y ** 2 - p4Y2)
+        g, h, i = p3X - p4X, p3Y - p4Y, (p3X ** 2 - p4X2) + (p3Y ** 2 - p4Y2)
+        det = a * (e * i - f * h) + b * (f * g - d * i) + c * (d * h - e * g)
+        return det > 0 and not isclose(d, 0)
+
     def is_inside(self, x: float, y: float) -> bool:
-        v0 = Point(self.P3.X - self.P1.X, self.P3.Y - self.P1.Y, 0)
-        v1 = Point(self.P2.X - self.P1.X, self.P2.Y - self.P1.Y, 0)
-        v2 = Point(x - self.P1.X, y - self.P1.Y, 0)
-        dot00 = v0.X ** 2 + v0.Y ** 2
-        dot01 = v0.X * v1.X + v0.Y * v1.Y
-        dot02 = v0.X * v2.X + v0.Y * v2.Y
-        dot11 = v1.X ** 2 + v1.Y ** 2
-        dot12 = v1.X * v2.X + v1.Y * v2.Y
+        v0X, v0Y = self.P3.X - self.P1.X, self.P3.Y - self.P1.Y
+        v1X, v1Y = self.P2.X - self.P1.X, self.P2.Y - self.P1.Y
+        v2X, v2Y = x - self.P1.X, y - self.P1.Y
+        dot00 = v0X ** 2 + v0Y ** 2
+        dot01 = v0X * v1X + v0Y * v1Y
+        dot02 = v0X * v2X + v0Y * v2Y
+        dot11 = v1X ** 2 + v1Y ** 2
+        dot12 = v1X * v2X + v1Y * v2Y
         inv_denom = 1.0 / (dot00 * dot11 - dot01 ** 2)
         u = (dot11 * dot02 - dot01 * dot12) * inv_denom
         v = (dot00 * dot12 - dot01 * dot02) * inv_denom
@@ -95,11 +111,11 @@ class DelaunayMap:
         max_x, max_y = max(p.X for p in points), max(p.Y for p in points)
         supertriangle = Triangle(Point(min_x - 1, min_y - 1, 0), Point(max_x * 3, min_y - 1, 0),
                                  Point(min_x - 1, max_y * 3, 0))
-        triangles = [supertriangle]
+        triangles = [supertriangle]  # type: List[Triangle]
         point_count = 0
         for p in points:
             point_count += 1
-            container_triangles = [tr for tr in triangles if tr.contains_in_circumcircle(p)]
+            container_triangles = [tr for tr in triangles if tr.contains_in_circumcircle(p)]  # type: List[Edge]
             edges = []  # type: List[Edge]
             for tr in container_triangles:
                 triangles.remove(tr)
@@ -112,11 +128,11 @@ class DelaunayMap:
                 raise Exception(
                     "Triangulation invariant violated! Triangle count and point count doesn't fit together.")
 
-        self._triangles = [tr for tr in triangles if not tr.shares_vertex(supertriangle)]
+        self._triangles = [tr for tr in triangles if not tr.shares_vertex(supertriangle)]  # type: List[Triangle]
 
     def __getitem__(self, xy: Tuple[float, float]) -> float:
         x, y = xy
-        for tr in self._triangles:  # type: Triangle
+        for tr in self._triangles:
             if tr.is_inside(x, y):
                 return tr.interpolate(x, y)
 
